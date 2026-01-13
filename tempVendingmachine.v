708d9ceac5278c0bd8cbdd768a0bc62e0e83c2a2
maitreyeecooolkarni <06maitreyeetemp@gmail.com>
2026-01-14 00:25:18 +0530

    Update tempVendingmachine.v

--- tempVendingmachine.v
+++ tempVendingmachine.v
@@ -1,6 +1,3 @@
-// Vending Machine
-
-// Vending Machine
 module vendingmachine (
     input        clk,
     input        P,
@@ -9,77 +6,97 @@ module vendingmachine (
     input        z2,
     input  [2:0] C,
     output reg   y1,
-    output reg   y2
+    output reg   y2,
+  output reg[2:0]change
 );
 
   reg [1:0] state;
   reg [1:0] nextstate;
   reg [2:0] productcost;
   reg [2:0] nxtstateR;
+  reg [2:0] moneycheck;
+  reg [2:0] nxtstateM;
+  
+  parameter S0 = 2'b00;
+  parameter S1 = 2'b01;
+  parameter S2 = 2'b10;
+  parameter S3 = 2'b11;
 
-  parameter S0 = 2'b00;  // idle state
-  parameter S1 = 2'b01;  // product selection state
-  parameter S2 = 2'b10;  // coin acceptance state
-
-  parameter R1 = 3'b010; // 2 rupees
-  parameter R2 = 3'b101; // 5 rupees
-  parameter R0 = 3'b000; // 5 rupees
+  parameter R1 = 3'b010;  //2 rupees
+  parameter R2 = 3'b101;  //5 rupees
+  parameter R0 = 3'b000;
 
   // Sequential block
   always @(posedge clk) begin
-    if(rst)
-      state<=S0;
+    if (rst)
+      state <= S0;
     else
-    state       <= nextstate;
+      state <= nextstate;
+
     productcost <= nxtstateR;
+    moneycheck  <= nxtstateM;
   end
 
   // Combinational block
   always @(*) begin
     nextstate  = state;
-    nxtstateR = productcost; //Default is added else a latch will be inferred
+    nxtstateR = productcost;
+    nxtstateM = moneycheck;
     y1 = 1'b0;
     y2 = 1'b0;
+    change = 3'b000;
 
     case (state)
-
       S0: begin
         if (P)
           nextstate = S1;
-      end  //No need to write S0 as already adressed in default
-        
+      end
 
       S1: begin
         if (z1) begin
           nxtstateR = R1;
           nextstate = S2;
-       end 
-        else begin
-          nextstate = S0;
         end
-        if (z2) begin
+        else if (z2) begin
           nxtstateR = R2;
           nextstate = S2;
-        end 
-        else begin
-          nextstate = S0;
-          nxtstateR = R0;
         end
-      end   
+        else begin
+          nxtstateR = R0;
+          nextstate = S0;
+        end
+      end
+
       S2: begin
         case (productcost)
           R1: begin
-            if (C >= R1)
-              y1 = 1'b1;
-            nextstate = S0;
+            if (C >= R1) begin
+              nxtstateM = C;
+              nextstate = S3;
+            end
           end
 
           R2: begin
-            if (C >= R2)
-              y2 = 1'b1;
-            nextstate = S0;
+            if (C >= R2) begin
+              nxtstateM = C;
+              nextstate = S3;
+            end
           end
         endcase
+      end
+
+      S3: begin
+        case (productcost)
+          R1: begin
+            change = moneycheck - R1;
+            y1 = 1'b1;
+          end
+          R2:begin 
+            change = moneycheck - R2;
+            y2 = 1'b1;
+          end
+        endcase
+        nextstate = S0;
       end
 
       default: begin
@@ -88,55 +105,5 @@ module vendingmachine (
 
     endcase
   end
-
 endmodule
-
-
-//Testbench
-`timescale 1ns/1ps
-
-module tb_vendingmachine;
-
-  reg        clk = 1'b0;
-  reg        P   = 1'b0;
-  reg        z1  = 1'b0;
-  reg        z2  = 1'b0;
-  reg [2:0]  C   = 3'b000;
-  reg rst = 1'b0;
-
-  wire y1;
-  wire y2;
-
-  vendingmachine dut (
-    .P   (P),
-    .clk (clk),
-    .C   (C),
-    .y1  (y1),
-    .y2  (y2),
-    .z1  (z1),
-    .z2  (z2),
-    .rst (rst)
-  );
-
-  always #5 clk = ~clk;
-
-initial begin
-  $monitor(
-    "t=%0t  | Product1=%b Product2=%b | State=%b | Dispense1=%b Dispense2=%b",
-    $time, z1, z2, dut.state, y1, y2
-  );
-  @(posedge clk) P  = 1'b1;
-  @(posedge clk) z1 = 1'b1;
-  @(posedge clk) C  = 3'b010;
-  
-  
-  @(posedge clk) P  = 1'b1;
-  @(posedge clk) z2 = 1'b1;
-  @(posedge clk) C  = 3'b101;
-  
-  
-  #10 $finish;
-end
-endmodule
-//will add more like give spare change and get a better verification where all inputs are checked
 
