ad34b40fc084d9febe746a19eeddafd414dec038
maitreyeecooolkarni <06maitreyeetemp@gmail.com>
2026-01-19 11:56:39 +0530

    Update Acummulatordatapath.v

--- Acummulatordatapath.v
+++ Acummulatordatapath.v
@@ -58,49 +58,4 @@ module controller(LdA,clr,clear);
     end
 endmodule
 
-    //data_in is basically the input 
   
-
-  PIPOA A(X,Z,clr,LdA,clk);
-
-  ADD AD(Z,in1,X);
-endmodule
-
-module PIPOA(dout,din,clr,ld,clk);
-  input [1:0] din;
-  input clk,clr,ld;
-  output reg [1:0]dout;
-  
-  always@(posedge clk)
-    begin
-    if(clr)
-      dout<=2'b00;
-  else if(ld)
-     dout<=din;
-    end
-endmodule
-
-
-module ADD(out,in1,in2);
-  input [1:0]in1,in2;
-  output [1:0] out;
-  
-  assign out=in1+in2;
-  
-endmodule
-
-module controller(LdA,clr,clear);
-  input clear;
-  output reg LdA,clr;
-  
-  always@(*)
-    begin
-      if(clear)begin
-      LdA=0;clr=1;
-      end
-    else
-      begin
-      LdA=1;clr=0;
-      end
-    end
-endmodule
